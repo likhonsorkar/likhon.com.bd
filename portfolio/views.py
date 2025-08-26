ceb013ff58702b960f945d6cccf9e8143ff9ada1
unknown <likhonsorkar002@gmail.com>
2025-08-26 11:00:11 +0600

    Some Work For Portfolio and main Home

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from portfolio.models import Service, Project, Review
 
 # Create your views here.
 def homepage(request):
-    return render(request, "home.html")
+    services = Service.objects.all()
+    projects = Project.objects.all()
+    reviews =  Review.objects.filter(is_approved=True)
+    context = {
+        'services': services,
+        'projects': projects,
+        'reviews': reviews,
+    }
+    return render(request, "home.html", context)
