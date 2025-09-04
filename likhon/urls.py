ac00725fed3ac8e7ca9283a4b1fce4d3880ed00d
unknown <likhonsorkar002@gmail.com>
2025-09-04 19:41:39 +0600

    Fresh

--- likhon/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from portfolio.views import *
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', homepage, name="homepage" ),
-    path('portfolio/', include('portfolio.urls')),
-    path('blog/', include('blog.urls')),
-]
