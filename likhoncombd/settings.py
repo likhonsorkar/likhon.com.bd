864aff1e4eee7b11b9efe1e8b704efaff10baee4
unknown <likhonsorkar002@gmail.com>
2025-09-06 13:01:19 +0600

    Static

--- likhoncombd/settings.py
+++ likhoncombd/settings.py
@@ -137,7 +137,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATIC_ROOT = BASE_DIR / "staticfiles"
 
 LOGIN_URL = 'signin'
 # Default primary key field type
